803a6cd0868a69461b23cae9d00447a2943881f6
ellemaj <loen0029@hz.nl>
2026-02-03 13:51:07 +0100

    basic framework structure with Kernel, Request, and Response classes

---
+++ src/Response.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Framework;
+
+class Response
+{
+    public int $responseCode;
+
+    public string $body;
+
+    public ?string $headers;
+
+    public function __construct(string $body, int $responseCode, ?string $headers = null)
+    {
+        $this->body = $body;
+        $this->responseCode = $responseCode;
+        $this->headers = $headers;
+    }
+
+    public function echo(): void
+    {
+        echo "You requested: " . $this-> headers . "with response code: " . $this-> responseCode . " " . $this->body;
+    }
+}
